65e11d94a912612051990ba3eb38a08ff6568fd4
Henrique <101234830+HOR93@users.noreply.github.com>
2023-04-20 21:57:17 -0300

    Update moore_mach.vhd

--- moore_mach.vhd
+++ moore_mach.vhd
@@ -1,16 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
 
-entity moore_machine is port (
+entity maquina_estados_moore is port (
+
     a : in std_logic_vector (1 downto 0);
+
     clk, reset : in std_logic;
-    r_soda, r_c25, r_c50 : out std_logic
-);
-end moore_machine;
 
-architecture arch of moore_machine is
+    refrigerante, r_c25, r_c50 : out std_logic );
 
-    type estado is (init, c25, c50, c75, c100, c125, d25, d50, d75);
+
+end maquina_estados_moore;
+
+architecture arch of maquina_estados_moore is
+
+    type estado is (init, 
+                    c25, 
+                    c50, 
+                    c75, 
+                    c100, 
+                    c125, 
+                    d25, 
+                    d50, 
+                    d75);
 
     signal current_state, next_state : estado;
 
@@ -28,94 +41,65 @@ begin
     begin
         case current_state is
             when init =>
-                r_soda <= '0';
-                r_c25 <= '0';
-                r_c50 <= '0';
+                refrigerante <= '0'; r_c25 <= '0'; r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
             when c25 =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '0';
                 r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= c25;
-                    when "01" => next_state <= c50;
-                    when "10" => next_state <= c75;
-                    when others => next_state <= d25;
+                    when "00" => next_state <= c25; when "01" => next_state <= c50; when "10" => next_state <= c75; when others => next_state <= d25;
                 end case;
             when c50 =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '0';
                 r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= c50;
-                    when "01" => next_state <= c75;
-                    when "10" => next_state <= c100;
-                    when others => next_state <= d50;
+                    when "00" => next_state <= c50; when "01" => next_state <= c75; when "10" => next_state <= c100; when others => next_state <= d50;
                 end case;
             when c75 =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '0';
                 r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= c75;
-                    when "01" => next_state <= c100;
-                    when "10" => next_state <= c125;
-                    when others => next_state <= d75;
+                    when "00" => next_state <= c75; when "01" => next_state <= c100; when "10" => next_state <= c125; when others => next_state <= d75;
                 end case;
             when c100 =>
-                r_soda <= '1';
+                refrigerante <= '1';
                 r_c25  <= '0';
                 r_c50  <= '0';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
             when c125 =>
-                r_soda <= '1';
+                refrigerante <= '1';
                 r_c25 <= '1';
                 r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
             when d25 =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '1';
                 r_c50 <= '0';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
             when d50 =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '0';
                 r_c50 <= '1';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
             when others =>
-                r_soda <= '0';
+                refrigerante <= '0';
                 r_c25 <= '1';
                 r_c50 <= '1';
                 case a is
-                    when "00" => next_state <= init;
-                    when "01" => next_state <= c25;
-                    when "10" => next_state <= c50;
-                    when others => next_state <= init;
+                    when "00" => next_state <= init; when "01" => next_state <= c25; when "10" => next_state <= c50; when others => next_state <= init;
                 end case;
         end case;
     end process;
